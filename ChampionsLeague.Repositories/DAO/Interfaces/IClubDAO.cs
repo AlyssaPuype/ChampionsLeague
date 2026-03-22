d2bcb55608c7bd93a67e50c344f9755bce21fe7a
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-22 14:58:53 +0100

    Create IClubDAO and ClubDAO

---
+++ ChampionsLeague.Repositories/DAO/Interfaces/IClubDAO.cs
@@ -0,0 +1,13 @@
+﻿using ChampionsLeague.Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChampionsLeague.Repositories.DAO.Interfaces
+{
+    public interface IClubDAO
+    {
+        Task<IEnumerable<Club>> GetAllClubsAsync();
+        Task<Club> GetByIdAsync(int id);
+    }
+}
