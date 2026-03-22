05ee4a1875c79dbd9968501b85a529da774fcda5
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-22 15:06:47 +0100

    Add IClubService and IClubService

---
+++ ChampionsLeague.Services/Services/ClubService.cs
@@ -0,0 +1,29 @@
+﻿using ChampionsLeague.Domains.Entities;
+using ChampionsLeague.Repositories.DAO.Interfaces;
+using ChampionsLeague.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChampionsLeague.Services
+{
+    public class ClubService : IClubService
+    {
+        private readonly IClubDAO _clubDAO;
+
+        public ClubService(IClubDAO clubDAO)
+        {
+            _clubDAO = clubDAO;
+        }
+
+        public async Task<IEnumerable<Club>> GetAllClubsAsync()
+        {
+            return await _clubDAO.GetAllClubsAsync();
+        }
+
+        public async Task<Club?> GetByIdAsync(int id)
+        {
+            return await _clubDAO.GetByIdAsync(id);
+        }
+    }
+}
---
+++ ChampionsLeague.Services/Services/Interfaces/IClubService.cs
@@ -0,0 +1,13 @@
+﻿using ChampionsLeague.Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChampionsLeague.Services.Services.Interfaces
+{
+    public interface IClubService
+    {
+        Task<IEnumerable<Club>> GetAllClubsAsync();
+        Task<Club?> GetByIdAsync(int id);
+    }
+}
