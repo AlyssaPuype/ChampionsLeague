091038dfe450ad85dc31f1913479a34831b73be3
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-22 15:12:53 +0100

    Add ClubController

---
+++ ChampionsLeague/Controllers/ClubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ChampionsLeague.Services.Services.Interfaces;
+
+
+namespace ChampionsLeague.Controllers
+{
+    public class ClubController : Controller
+    {
+
+        private readonly IClubService _clubService;
+
+        public ClubController(IClubService clubService)
+        {
+            _clubService = clubService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
