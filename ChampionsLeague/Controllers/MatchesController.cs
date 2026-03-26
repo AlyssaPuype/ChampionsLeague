f99c8dc567dc104966cd9d452aefef1d68c54194
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-26 17:51:39 +0100

    #32 Set DAO and interface DAO for match

---
+++ ChampionsLeague/Controllers/MatchesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChampionsLeague.Controllers
+{
+    public class MatchesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ChampionsLeague/Views/Matches/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChampionsLeague.Views.Matches
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
