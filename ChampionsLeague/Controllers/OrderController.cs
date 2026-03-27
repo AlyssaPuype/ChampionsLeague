665320d749b7a092256f0cf6f7a53aa31ffe113f
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-27 17:45:49 +0100

    Adapt controller

--- ChampionsLeague/Controllers/OrderController.cs
+++ ChampionsLeague/Controllers/OrderController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChampionsLeague.Domains.Entities;
+using ChampionsLeague.Models.Order;
+using ChampionsLeague.Services.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChampionsLeague.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         public IActionResult Index()
