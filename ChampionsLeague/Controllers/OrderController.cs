bee551de56867728fa37f1ff06b0e19861577237
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-27 17:42:02 +0100

    Create TicketVM

---
+++ ChampionsLeague/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChampionsLeague.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
