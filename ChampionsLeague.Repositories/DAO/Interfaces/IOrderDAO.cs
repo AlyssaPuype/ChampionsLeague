288217800238c6407f35cfe2d6f7cbbde4a9cfd1
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-27 17:55:58 +0100

    Create interface for order

---
+++ ChampionsLeague.Repositories/DAO/Interfaces/IOrderDAO.cs
@@ -0,0 +1,14 @@
+﻿using ChampionsLeague.Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChampionsLeague.Repositories.DAO.Interfaces
+{
+    public interface IOrderDAO
+    {
+        Task<IEnumerable<Order>> GetAllAsync();
+        Task<Order?> GetByIdAsync(int id);
+        Task<IEnumerable<Order>> GetByUserAsync(string userId);
+    }
+}
