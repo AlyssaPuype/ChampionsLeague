a7d30fbbe08a641d755a2db9290cdba7a6eb67ef
AlyssaPuype <alyssa.puype@gmail.com>
2026-03-28 15:23:56 +0100

    Fix applicationDbcontext

--- ChampionsLeague/Data/ApplicationDbContext.cs
+++ ChampionsLeague/Data/ApplicationDbContext.cs
@@ -4,7 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ChampionsLeague.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
     }
+    
 }
