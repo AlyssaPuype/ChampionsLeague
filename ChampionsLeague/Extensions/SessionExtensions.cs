9ad432978888fa0eee7f3a7c58edbd2974a22451
AlyssaPuype <alyssa.puype@gmail.com>
2026-04-01 21:07:34 +0200

    Add Extension class

---
+++ ChampionsLeague/Extensions/SessionExtensions.cs
@@ -0,0 +1,27 @@
+﻿//extension class for shopping cart
+
+
+using Newtonsoft.Json;
+
+namespace ChampionsLeague.Extensions
+{
+    public static class SessionExtensions
+    {
+
+
+        public static void SetObject(this ISession session, string key, object? value)
+        {
+
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T? GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key); 
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+            //}
+        }
+
+
+    }
+}
