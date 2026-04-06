efb65385c2fb44ded927bb872fdbcd59b2b4fb66
AlyssaPuype <alyssa.puype@gmail.com>
2026-04-06 14:54:31 +0200

    Add emailservice for identity (forgot password)

---
+++ ChampionsLeague.Util/Mail/IdentityEmailSend.cs
@@ -0,0 +1,21 @@
+﻿using ChampionsLeague.Util.Mail.Interfaces;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChampionsLeague.Util.Mail
+{
+    public class IdentityEmailSend : IEmailSender
+    {
+        private readonly IEmailSend _emailSend;
+        public IdentityEmailSend(IEmailSend emailSend)
+        {
+            _emailSend = emailSend;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            await _emailSend.SendEmailAsync(email, subject, htmlMessage);
+        }
+    }
+}
