afc4e361513cf0b3a046ffc0e7c27d8a35372dfa
AlyssaPuype <alyssa.puype@gmail.com>
2026-04-19 20:33:00 +0200

    #6 remove datatables - change to list and add filter dropdown

--- ChampionsLeague/Controllers/MatchController.cs
+++ ChampionsLeague/Controllers/MatchController.cs
@@ -1,23 +1,32 @@
-﻿using ChampionsLeague.Services.Services.Interfaces;
+﻿using ChampionsLeague.Domains.Entities;
+using ChampionsLeague.Services.Services.Interfaces;
 using ChampionsLeague.Services.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ChampionsLeague.Web.Controllers
 {
     public class MatchController : Controller
     {
         private readonly IMatchService _matchService;
+        private readonly IClubService _clubService;
 
-        public MatchController(IMatchService matchService)
+        public MatchController(IMatchService matchService, IClubService clubService)
         {
             _matchService = matchService;
+            _clubService = clubService;
         }
 
         // get matches
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? clubId)
         {
-            var matches = await _matchService.GetAllMatchesAsync();
+            var matches = clubId.HasValue
+                ? await _matchService.GetMatchesByClubAsync(clubId.Value)
+                : await _matchService.GetAllMatchesAsync();
+
+            var clubs = await _clubService.GetAllClubsAsync();
+            ViewBag.Clubs = new SelectList(clubs, "Id", "Naam", clubId);
             return View(matches);
         }
 
