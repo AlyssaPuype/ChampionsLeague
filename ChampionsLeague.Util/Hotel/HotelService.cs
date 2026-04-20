6a85a4562f527161d23e30ca536b8064ab659bcd
AlyssaPuype <alyssa.puype@gmail.com>
2026-04-20 11:30:08 +0200

    Fixed

--- ChampionsLeague.Util/Hotel/HotelService.cs
+++ ChampionsLeague.Util/Hotel/HotelService.cs
@@ -102,29 +102,29 @@ namespace ChampionsLeague.Util.Hotel
                 //source: https://learn.microsoft.com/en-us/dotnet/api/system.text.json.jsonelement.trygetproperty?view=net-10.0
                  hotels.Add(new HotelResultVM
                 {
-                    HotelId = hotel.TryGetProperty("hotel_id", out var id)
-                        ? id.GetInt32() : 0,
-                    Name = hotel.TryGetProperty("hotel_name", out var naam)
-                        ? naam.GetString() ?? "Unknown" : "Unknown",
-                    Address = hotel.TryGetProperty("address", out var adres)
-                        ? adres.GetString() : null,
-                    City = hotel.TryGetProperty("city", out var stad)
-                        ? stad.GetString() : null,
-                    Country = hotel.TryGetProperty("country_trans", out var land)
-                        ? land.GetString() : null,
-                    ReviewScore = hotel.TryGetProperty("review_score", out var score) && score.ValueKind != JsonValueKind.Null
-                        ? score.GetDouble() : null,
-                    ReviewCount = hotel.TryGetProperty("review_nr", out var nr) && score.ValueKind != JsonValueKind.Null
-                        ? nr.GetInt32() : null,
-                    ReviewWord = hotel.TryGetProperty("review_score_word", out var word)
-                        ? word.GetString() : null,
-                    Stars = hotel.TryGetProperty("class", out var stars) && score.ValueKind != JsonValueKind.Null
-                        ? stars.GetDouble() : null,
-                    PhotoUrl = hotel.TryGetProperty("main_photo_url", out var foto)
-                        ? foto.GetString()?.Replace("square60", "square500") : null,
-                    Url = hotel.TryGetProperty("url", out var hotelUrl)
-                        ? hotelUrl.GetString() : null
-                });
+                    HotelId = hotel.TryGetProperty("hotel_id", out var id) ? id.GetInt32() : 0,
+
+                     Name = hotel.TryGetProperty("hotel_name", out var naam) ? naam.GetString() ?? "Unknown" : "Unknown",
+
+                     Address = hotel.TryGetProperty("address", out var adres) ? adres.GetString() : null,
+
+                     City = hotel.TryGetProperty("city", out var stad) ? stad.GetString() : null,
+
+                     Country = hotel.TryGetProperty("country_trans", out var land) ? land.GetString() : null,
+
+                     ReviewScore = hotel.TryGetProperty("review_score", out var score) && score.ValueKind != JsonValueKind.Null ? score.GetDouble() : null,
+
+                     ReviewCount = hotel.TryGetProperty("review_nr", out var nr) && score.ValueKind != JsonValueKind.Null ? nr.GetInt32() : null,
+
+                     ReviewWord = hotel.TryGetProperty("review_score_word", out var word) ? word.GetString() : null,
+
+                     Stars = hotel.TryGetProperty("class", out var stars) && score.ValueKind != JsonValueKind.Null ? stars.GetDouble() : null,
+
+                     PhotoUrl = hotel.TryGetProperty("main_photo_url", out var foto) ? foto.GetString()?.Replace("square60", "square500") : null,
+
+                     Url = hotel.TryGetProperty("url", out var hotelUrl) ? hotelUrl.GetString() : null
+
+                 });
             }
 
             return hotels;
