2bc6bc2034b94ea87435ac25336cb28ef52756eb
AlyssaPuype <alyssa.puype@gmail.com>
2026-04-21 13:58:51 +0200

    beschrijving geplaatst

--- ChampionsLeague.Repositories/DAO/ZitplaatsDAO.cs
+++ ChampionsLeague.Repositories/DAO/ZitplaatsDAO.cs
@@ -17,36 +17,42 @@ namespace ChampionsLeague.Repositories.DAO
             _context = context;
         }
 
-        //Get alle beschikbare zitplaatsen
+        //Tickets
+        //Gebruiker kiest een match, een vak en het aantal gewenste zitplaatsen
+        //Return: Lijst van zitplaatsen
         public async Task<IEnumerable<Zitplaats>> GetBeschikbareZitplaatsenVoorTicketAsync(int matchId, int stadionvakId, int aantalGewensteZitplaatsen)
         {
             return await _context.Zitplaatsen
                 .Where(z => z.StadionvakId == stadionvakId)
-                .Where(z => !z.Tickets.Any(t => t.MatchId == matchId && t.Status != "geannuleerd")) 
-                .Where(z => !_context.Abonnements.Any(a => a.ZitplaatsId == z.Id)) //R-6
+                .Where(z => !z.Tickets.Any(t => t.MatchId == matchId && t.Status != "geannuleerd")) //Sluit stoelen uit waarvan tickets al gekocht zijn
+                .Where(z => !_context.Abonnements.Any(a => a.ZitplaatsId == z.Id)) //R-6 Sluit stoelen uit die een abonnement hebben
                 .Take(aantalGewensteZitplaatsen)
                 .ToListAsync();
         }
 
-        //Get aantal beschikbare zitplaatsen voor een stadionvak
-        public async Task<int> GetAantalBeschikbareZitplaatsen(int stadionvakId, int matchId)
-        {
-            return await _context.Zitplaatsen
-                .Where(z => z.StadionvakId == stadionvakId)
-                .Where(z => !_context.Abonnements.Any(a => a.ZitplaatsId == z.Id))
-                .Where(z => matchId == 0 || !z.Tickets.Any(t => t.MatchId == matchId && t.Status != "geannuleerd"))
-                .CountAsync();
-        }
-
-        //Get 1 beschikbare zitplaats voor abonnement
+        //Abonnementen
+        //Gebruiker kiest een vak en krijgt de eerste vrije zitplaats in dat vak toegewezen
+        //Return: 1 zitplaats
         public async Task<Zitplaats?> GetBeschikbareZitplaatsVoorAbonnementAsync(int stadionvakId)
         {
            
             return await _context.Zitplaatsen
-                .Where(z => z.StadionvakId == stadionvakId
-                    && !_context.Abonnements.Any(a => a.ZitplaatsId == z.Id)
-                    && !_context.Tickets.Any(t => t.ZitplaatsId == z.Id && t.Status != "geannuleerd")) //R-7
+                .Where(z => z.StadionvakId == stadionvakId)
+                .Where(z => !_context.Abonnements.Any(a => a.ZitplaatsId == z.Id)) // Sluit stoelen uit die al een abonnement hebben
+                .Where(z => !_context.Tickets.Any(t => t.ZitplaatsId == z.Id && t.Status != "geannuleerd")) //R-7 // Sluit stoelen uit die al een actief ticket hebben
                 .FirstOrDefaultAsync();
         }
+
+        //Get aantal vrije zitplaatsen
+        //Return: int
+        public async Task<int> GetAantalBeschikbareZitplaatsen(int stadionvakId, int matchId)
+        {
+            return await _context.Zitplaatsen
+                .Where(z => z.StadionvakId == stadionvakId)
+                .Where(z => !_context.Abonnements.Any(a => a.ZitplaatsId == z.Id)) //Sluit stoelen uit die een abonnement hebben
+                .Where(z => matchId == 0 || !z.Tickets.Any(t => t.MatchId == matchId && t.Status != "geannuleerd")) // Get alle zitplaatsen die geen abonnement hebben of sluit tickets uit die een zitplaats hebben
+                .CountAsync();
+        }
+
     }
 }
